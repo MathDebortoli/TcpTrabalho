8b5c562dfb70fd5f1730ce4a9a8567241c90b9c5
klitzq <joaov.klitzke@outlook.com>
2024-10-02 13:26:36 -0300

    Alterando nome do pacote

--- TcpCliente/src/trabalhosd/TcpServidor.java
+++ TcpCliente/src/trabalhosd/TcpServidor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tcpcliente;
+package trabalhosd;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
